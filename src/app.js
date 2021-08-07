f3b0f1003a2c30f4d1b95ac06e5585fb7b9768be
kiddopro <ms.prgramador@gmail.com>
2021-08-07 01:13:04 -0300

    done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ['The dog','My grandma','His turtle','My bird'];
+let action = ['ate','peed','crushed','broke'];
+let what = ['my homework', 'the keys', 'the car'];
+let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let element = document.getElementById("excuse");
+  element.innerHTML = phrase();
 };
+
+function randomExcuses(arrayOfExcuses) {
+  let random = Math.floor(Math.random() * arrayOfExcuses.length - 1) + 1;
+  return random;
+}
+
+function phrase() {
+  let phrase =
+    who[randomExcuses(who)] +
+    " " +
+    action[randomExcuses(action)] +
+    " " +
+    what[randomExcuses(what)] +
+    " " +
+    when[randomExcuses(when)];
+  return phrase;
+}
+
+
